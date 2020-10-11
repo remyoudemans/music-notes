5f0b74ded09d263886966dfae0557a095ae4080a
remyoudemans <remyoudemans@gmail.com>
2020-10-11 12:39:30 +0100

    feat: make square brackets sound accidentals on input

--- src/Staff/index.js
+++ src/Staff/index.js
@@ -4,7 +4,7 @@ import { Synth } from 'tone';
 const range = x => [...Array(x).keys()];
 const trebleClefPath ="M15.1 65.4c-4.2-.3-8.5-3-11.1-6.1-3-3.7-4.3-8.6-4-12.7 1-12 13.6-19 18.3-24.3 3.1-3.5 3.8-5.2 4.5-7 1.3-3.5 1.6-7.5-1.1-7.8-2.6-.3-4.7 3.8-5.6 6.7-.9 2.5-1.4 5-1 8.9l6.7 50.7c1 7.5-3 10.5-7.3 11.1-9.2 1.2-12.1-8.4-8-12 3.2-2.8 7.7-.4 7.5 4.2-.2 4-4.1 4.2-5.1 4 1.5 2.9 13 4.4 11.2-8.3L14 24.7C13 17 12.7 11 16.5 4c1.5-2.6 3.7-4.3 4.8-4 .2 0 .5 0 .7.3 3 3 3.8 10 3.5 14-.3 4-.5 8.3-4.5 13.2-1.5 2-6.1 6.1-8.8 8.5a28.6 28.6 0 00-8 10A12.6 12.6 0 0014.8 64c8.4 0 10.8-4.2 11-9 0-7.9-9.2-10.8-13-5.4a6.4 6.4 0 00.2 8c.6.6 1.1 1 1.7 1.2.2 0 .6.3.5.6-.1.3-.3.3-.5.3-2.4-.3-5-2.7-5.7-6.7-1-6 4-13 11.3-11.8 4.8.7 9.2 5 8.8 12.7-.4 6.7-5.8 12-13.9 11.5z";
 
-const noteNames = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
+export const noteNames = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
 
 class Staff {
   constructor(canvas, x, y, lineGap = 10, lineLength = 500) {
@@ -32,28 +32,6 @@ class Staff {
       this.canvas[this.showCursor ? 'add' : 'remove'](this.cursor);
       this.showCursor = !this.showCursor;
     }, 500)
-
-    document.addEventListener('keydown', e => {
-      if (e.key === 'n') {
-        this.upOctave();
-      } else if (e.key === 'p') {
-        this.downOctave();
-      }
-
-      const uppercaseKey = e.key.toUpperCase();
-
-      if (noteNames.includes(uppercaseKey)) {
-        this.drawNote(uppercaseKey)
-      }
-    });
-
-    document.addEventListener('keyup', e => {
-      if (e.key === 'n') {
-        this.downOctave();
-      } else if (e.key === 'p') {
-        this.upOctave();
-      }
-    })
   }
 
   upOctave() {
@@ -94,7 +72,7 @@ class Staff {
     ));
   }
 
-  drawNote(note) {
+  drawNote(note, accidental = '') {
     const noteY = this.getNoteY(note);
     const noteX = this.x + this.noteOffset;
     const width = this.lineGap / 2;
@@ -110,7 +88,7 @@ class Staff {
     );
 
     this.canvas.add(new fabric.Group([circle, line]));
-    this.synth.triggerAttackRelease(`${note}${this.octave}`, "32n");
+    this.synth.triggerAttackRelease(`${note}${accidental}${this.octave}`, "32n");
 
     this.noteOffset += 20;
     this.cursor.left = this.x + this.noteOffset;
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,9 +3,11 @@
   import { fabric } from 'fabric';
 
   import InputBar from '../components/InputBar.svelte'
-  import Staff from '../Staff';
+  import Staff, { noteNames } from '../Staff';
 
   let pressedKey;
+
+  const extendedCommandKeys = ['[', ']'];
 
   onMount(() => {
     const canvas = new fabric.Canvas('canvas');
@@ -15,7 +17,42 @@
     staff.drawClef();
 
     document.addEventListener('keydown', e => {
-      pressedKey = e.key;
+      if (extendedCommandKeys.includes(pressedKey)) {
+        pressedKey += e.key;
+      } else {
+        pressedKey = e.key;
+      }
+
+      if (e.key === 'n') {
+        staff.upOctave();
+      } else if (e.key === 'p') {
+        staff.downOctave();
+      }
+
+      const uppercaseKey = e.key.toUpperCase();
+
+      if (noteNames.includes(uppercaseKey)) {
+        const accidental = (() => {
+          if (pressedKey.startsWith('[')) {
+            return 'b';
+          }
+
+          if (pressedKey.startsWith(']')) {
+            return '#';
+          }
+
+          return undefined;
+        })()
+        staff.drawNote(uppercaseKey, accidental);
+      }
+    });
+
+    document.addEventListener('keyup', e => {
+      if (e.key === 'n') {
+        staff.downOctave();
+      } else if (e.key === 'p') {
+        staff.upOctave();
+      }
     })
   });
 
