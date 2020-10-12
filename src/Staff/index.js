b99ca3fd00ef1b420709870aabecea41f25cd453
remyoudemans <remyoudemans@gmail.com>
2020-10-12 20:48:39 +0100

    feat: add flat graphically

--- src/Staff/index.js
+++ src/Staff/index.js
@@ -6,6 +6,8 @@ const trebleClefPath ="M15.1 65.4c-4.2-.3-8.5-3-11.1-6.1-3-3.7-4.3-8.6-4-12.7 1-
 
 const sharpPath = "M1.9 12.15v-4.7l2-.55v4.68l-2 .57zm3.94-1.13l-1.37.39V6.73l1.37-.38V4.4l-1.37.39V0H3.9v4.93l-2 .58V.86h-.53v4.82L0 6.07v1.95l1.38-.39v4.67L0 12.7v1.94l1.38-.39V19h.53v-4.93l2-.55v4.63h.56v-4.8l1.37-.39v-1.94z";
 
+const flatPath = "M3.22 8.38c0 .57-.22 1.12-.8 1.86A9.36 9.36 0 01.55 12V8.57c.16-.4.4-.72.7-.97.31-.25.62-.37.94-.37.52 0 .85.3 1 .88l.02.27zm-.08-2.4c-.43 0-.87.12-1.31.35a5.4 5.4 0 00-1.27.96V.02H0v12.45c0 .35.1.53.29.53.1 0 .25-.1.45-.22.59-.34.95-.58 1.34-.82.46-.28.96-.6 1.63-1.25.47-.46.8-.93 1.01-1.4.2-.48.31-.95.31-1.41 0-.69-.18-1.18-.55-1.47-.41-.3-.86-.45-1.34-.45z"
+
 export const noteNames = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
 
 class Staff {
@@ -74,15 +76,35 @@ class Staff {
     ));
   }
 
+  drawSharp(noteX, noteY) {
+    this.canvas.add(
+      new fabric.Path(sharpPath, {
+        left: noteX,
+        top: noteY - 5,
+      })
+    );
+  }
+
+  drawFlat(noteX, noteY) {
+    this.canvas.add(
+      new fabric.Path(flatPath, {
+        left: noteX,
+        top: noteY - 9,
+        scaleY: 1.5,
+      })
+    );
+  }
+
   drawNote(note, accidental = '') {
     const noteY = this.getNoteY(note);
     let noteX = this.x + this.noteOffset;
 
-    if (accidental === '#') {
-      // TODO: try to get rid of all these magic numbers
-      this.canvas.add(
-        new fabric.Path(sharpPath, { left: noteX, top: noteY - 5})
-      );
+    if (accidental) {
+      if (accidental === '#') {
+        this.drawSharp(noteX, noteY);
+      } else {
+        this.drawFlat(noteX, noteY)
+      }
 
       noteX += 10;
       this.noteOffset += 10;
